0f151228583ba69014fe7f3157a8de63485d78e6
osypiukjakub <osypiuk.jakub@gmail.com>
2024-04-06 09:25:15 +0200

    ZAD2

--- Main.java
+++ Main.java
@@ -1,35 +1,17 @@
-public class Point {
-    private double x;
-    private double y;
-
-    public Point(double x, double y) {
-        this.x = x;
-        this.y = y;
-    }
-    
-
-    public void setX(double x) {
-        this.x = x;
-    }
-    public void setY(double y) {
-        this.y = y;
-    }
-    public double getX() {
-        return x;
-    }
-    public double getY() {
-        return y;
-    }
-
-}
-
+import geometry.Circle;
+import geometry.Point;
 
 public class Main {
     public static void main(String[] args) {
-        // Tworzenie obiektu klasy Point i inicjowanie go wartościami
         Point point = new Point(4.0, 2.0);
-        
-        // Wyświetlanie współrzędnych punktu
         System.out.println("Wspolrzedne dla punktu (" + point.getX() + ", " + point.getY() + ")");
+
+        Circle circle = new Circle(point, 7.0);
+        
+    
+        System.out.println("obwod: " + circle.calculatePerimeter());
+        System.out.println("srodek: (" + circle.getCenter().getX() + ", " + circle.getCenter().getY() + ")");
+        System.out.println("promien: " + circle.getRadius());
+        
     }
 }
